63b0c481436cab068357d80a00c7ffe3eebf1590
innaUKR <innascherbak11@gmail.com>
2018-02-13 17:48:38 +0200

    replace dimension with height,width and depth in book

---
+++ db/migrate/20180213133157_remove_dimension_from_books.rb
@@ -0,0 +1,5 @@
+class RemoveDimensionFromBooks < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :books, :dimension
+  end
+end
---
+++ db/migrate/20180213134036_add_columns_to_book.rb
@@ -0,0 +1,7 @@
+class AddColumnsToBook < ActiveRecord::Migration[5.1]
+  def change
+    add_column :books, :height, :decimal, precision: 5, scale: 2
+    add_column :books, :width, :decimal, precision: 5, scale: 2
+    add_column :books, :depth, :decimal, precision: 5, scale: 2
+  end
+end
