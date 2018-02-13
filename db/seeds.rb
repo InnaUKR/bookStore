de42c9e8a3c13cec2063d7decd8cd5b73cbcb1ca
innaUKR <innascherbak11@gmail.com>
2018-02-13 18:11:39 +0200

    add book in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'Real-Life Responsive Web Design',
+            price: 32.90,
+            quantity: 3,
+            description:
+                %(Responsive design is a default these days, but we are all still figuring out just the right process
+                  and techniques to better craft responsive websites. That’s why Smashing Magazine created a new book —
+                  to gather practical techniques and strategies from people who have learned how to get things done
+                  right, in actual projects with actual real-world challenges.
+
+                  The Smashing Book 5: Real-Life Responsive Web Design is Smashing Magazine’s brand new book with smart
+                  front-end techniques and design patterns derived from real-life responsive projects. Part 1 features
+                  7 chapters on responsive workflow, SVG, Flexbox, content strategy, and design patterns — just what
+                  you need to master all the tricky facets and hurdles of responsive design.
+
+                  Written by Daniel Mall, Ben Callahan, Eileen Webb, Sara Soueidan, Vitaly Friedman and Zoe M. Gillenwater.
+
+                  Please note that the corresponding Part 2 is also available with even more responsive web design tips
+                  and tricks — among others on web fonts, responsive images, email design, performance, debugging and
+                  optimizing for offline.),
+            year: Date.new(2016),
+            height: 6.4,
+            width: 0.9,
+            depth: 5.0,
+            material: 'Hardcove, glossy paper')
