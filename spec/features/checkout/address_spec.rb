517e9a5f4f3f51ba73bd0ca08fa7c91a8065dd33
innaUKR <innascherbak11@gmail.com>
2018-10-07 11:45:21 +0300

    Refactoring address spec

--- spec/features/checkout/address_spec.rb
+++ spec/features/checkout/address_spec.rb
@@ -11,13 +11,7 @@ feature 'At the Checkout page, the Address tab' do
       visit edit_order_path(line_item.order)
       expect(page).to have_current_path('/checkouts/address')
 
-      fill_in 'address_billing_address_form_first_name', with: address.first_name
-      fill_in 'address_billing_address_form_last_name', with: address.last_name
-      fill_in 'address_billing_address_form_address', with: address.address
-      fill_in 'address_billing_address_form_city', with: address.city
-      fill_in 'address_billing_address_form_zip', with: address.zip
-      fill_in 'address_billing_address_form_country', with: address.country
-      fill_in 'address_billing_address_form_phone', with: address.phone
+      fill_billing_address_form(address)
       first("input[name='address[use_billing]']", visible: false).set "true"
       click_on 'Save And Continue'
 
@@ -29,23 +23,10 @@ feature 'At the Checkout page, the Address tab' do
       visit edit_order_path(line_item.order)
       expect(page).to have_current_path('/checkouts/address')
 
-      fill_in 'address_billing_address_form_first_name', with: address.first_name
-      fill_in 'address_billing_address_form_last_name', with: address.last_name
-      fill_in 'address_billing_address_form_address', with: address.address
-      fill_in 'address_billing_address_form_city', with: address.city
-      fill_in 'address_billing_address_form_zip', with: address.zip
-      fill_in 'address_billing_address_form_country', with: address.country
-      fill_in 'address_billing_address_form_phone', with: address.phone
-
-      fill_in 'address_shipping_address_form_first_name', with: other_address.first_name
-      fill_in 'address_shipping_address_form_last_name', with: other_address.last_name
-      fill_in 'address_shipping_address_form_address', with: other_address.address
-      fill_in 'address_shipping_address_form_city', with: other_address.city
-      fill_in 'address_shipping_address_form_zip', with: other_address.zip
-      fill_in 'address_shipping_address_form_country', with: other_address.country
-      fill_in 'address_shipping_address_form_phone', with: other_address.phone
-
+      fill_billing_address_form(address)
+      fill_shipping_address_form(other_address)
       click_on 'Save And Continue'
+
       expect(page).to have_current_path('/checkouts/delivery')
     end
 
@@ -55,15 +36,9 @@ feature 'At the Checkout page, the Address tab' do
       visit edit_order_path(line_item.order)
       expect(page).to have_current_path('/checkouts/address')
 
-      fill_in 'address_billing_address_form_first_name', with: address.first_name
-      fill_in 'address_billing_address_form_last_name', with: address.last_name
-      fill_in 'address_billing_address_form_address', with: address.address
-      fill_in 'address_billing_address_form_city', with: address.city
-      fill_in 'address_billing_address_form_zip', with: address.zip
-      fill_in 'address_billing_address_form_country', with: address.country
-      fill_in 'address_billing_address_form_phone', with: address.phone
-
+      fill_billing_address_form(address)
       click_on 'Save And Continue'
+
       expect(page).to have_current_path('/checkouts/address')
       expect(find('#order_shipping_address')).to have_content('can\'t be blank, is invalid')
     end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -5,4 +5,24 @@ module FeatureHelper
     fill_in 'Password', with: user.password
     click_on 'Back to Store'
   end
+
+  def fill_billing_address_form(address)
+    fill_in 'address_billing_address_form_first_name', with: address.first_name
+    fill_in 'address_billing_address_form_last_name', with: address.last_name
+    fill_in 'address_billing_address_form_address', with: address.address
+    fill_in 'address_billing_address_form_city', with: address.city
+    fill_in 'address_billing_address_form_zip', with: address.zip
+    fill_in 'address_billing_address_form_country', with: address.country
+    fill_in 'address_billing_address_form_phone', with: address.phone
+  end
+
+  def fill_shipping_address_form(address)
+    fill_in 'address_shipping_address_form_first_name', with: address.first_name
+    fill_in 'address_shipping_address_form_last_name', with: address.last_name
+    fill_in 'address_shipping_address_form_address', with: address.address
+    fill_in 'address_shipping_address_form_city', with: address.city
+    fill_in 'address_shipping_address_form_zip', with: address.zip
+    fill_in 'address_shipping_address_form_country', with: address.country
+    fill_in 'address_shipping_address_form_phone', with: address.phone
+  end
 end
